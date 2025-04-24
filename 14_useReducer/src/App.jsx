6c8ef1ccb561d95a73e26156c2d6b2889dbd1537
jinukrishnan <jinukrishnan.p@gmail.com>
2025-04-24 14:54:22 +0530

    usereducerupdate

--- 14_useReducer/src/App.jsx
+++ 14_useReducer/src/App.jsx
@@ -3,25 +3,40 @@ import React, { useReducer } from 'react'
 
 const count=0
 const reducer=(state,action)=>{
-  switch(action){
+  switch(action.type){
     case "increment":
-      return state+1
+      return state+action.value
     case "decrement":
-      return state-1
+      return state-action.value
     case "reset":
-      return count
+      return action.value
     default:
       return state
   }
 }
+// const reducer=(state,action)=>{
+//   switch(action){
+//     case "increment":
+//       return state+1
+//     case "decrement":
+//       return state-1
+//     case "reset":
+//       return count
+//     default:
+//       return state
+//   }
+// }
 const App = () => {
   const [state,dispatch]=useReducer(reducer,count)
   return (
     <div>
       <h1>{state}</h1>
-      <button onClick={()=>dispatch("increment")}>increment</button>
+      <button onClick={()=>dispatch({type:"increment",value:5})}>increment</button>
+      <button onClick={()=>dispatch({type:"decrement",value:5})}>decrement</button>
+      <button onClick={()=>dispatch({type:"reset",value:0})}>reset</button>
+      {/* <button onClick={()=>dispatch("increment")}>increment</button>
       <button onClick={()=>dispatch("decrement")}>decrement</button>
-      <button onClick={()=>dispatch("reset")}>reset</button>
+      <button onClick={()=>dispatch("reset")}>reset</button> */}
     </div>
   )
 }
